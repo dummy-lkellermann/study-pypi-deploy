a255fcfd7c9e938893597d8c57894d3f91318b4b
Leandro Kellermann de Oliveira <lkellermann@leandroasaservice.com>
2025-09-03 09:55:56 -0300

    Attempt to fix coverage result difference + building venv and chaching it + replacing dependency of build/setup shell script with task command.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,9 +1,3 @@
-import sys
-
-sys.path.append('./src')
-
 def test_add_one():
-    for p in sys.path:
-        print(p)
     from study_pypi_deploy.example import add_one
     assert add_one(1) == 2
