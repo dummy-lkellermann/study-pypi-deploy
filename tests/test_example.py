59a278d1aced85268418464daf7be3ef1d6c0190
Leandro Kellermann de Oliveira <lkellermann@leandroasaservice.com>
2025-09-03 11:41:40 -0300

    testing if changes in test_example.py will trigger workflow.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,7 @@
 def test_add_one():
     from study_pypi_deploy.example import add_one
     assert add_one(1) == 2
+
+def test_divide():
+    from study_pypi_deploy.example import divide
+    assert divide(6, 3) == 2
